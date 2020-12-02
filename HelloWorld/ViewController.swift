28669a503bc1deea14fbfa96cd2d7695cd69421e
Andrey Terechshenkov <aterechshenkov@gmail.com>
2020-12-02 10:36:16 +0600

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Terechshenkov Andrey on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
